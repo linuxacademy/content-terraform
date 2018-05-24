1b089c39feaf344c136da501537c7eea465a5c32
EC2 Default User <ec2-user@ip-172-31-45-116.us-west-2.compute.internal>
2018-05-24 06:31:24 +0000

    fixed default_route_table_id issue #1

--- course/terraform-aws/networking/main.tf
+++ course/terraform-aws/networking/main.tf
@@ -34,7 +34,7 @@ resource "aws_route_table" "tf_public_rt" {
 }
 
 resource "aws_default_route_table" "tf_private_rt" {
-  default_route_table_id = "{aws_vpc.tf_vpc.default_route_table.id}"
+  default_route_table_id = "${aws_vpc.tf_vpc.default_route_table_id}"
 
   tags {
     Name = "tf_private"
